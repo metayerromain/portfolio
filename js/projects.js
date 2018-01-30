355937f1688e7000e3743cc30b5124f03a644d4f
Romain Metayer <33374185+metayerromain@users.noreply.github.com>
2018-01-30 23:43:15 +0100

    Add files via upload

--- js/projects.js
+++ js/projects.js
@@ -5,12 +5,12 @@ var body = document.querySelector("body");
  */
 setTimeout(function() {
   //La page chargé on enlève le loader
+  //La page chargé on enlève le loader
   var loader = document.querySelector(".loader");
   loader.classList.add("loader--animation");
   setTimeout(function() {
     loader.style.display = "none";
   }, 500);
-
   //Animation du contenu
   var companyTitle = document.querySelector(".companyTitle");
   var containerInfosProject = document.querySelector(".containerInfosProject");
@@ -36,60 +36,4 @@ footer.addEventListener("mouseover", function() {
 });
 footer.addEventListener("mouseout", function() {
   square.classList.toggle("squareMoveAnimation--animation");
-});
-
-
-var spam = false;
-var slideProject = document.querySelectorAll(".slideProject");
-//ONSCROLL
-window.addEventListener("mousewheel", function(e) {
-  var activeSlide = document.querySelector(".activeSlideProject");
-  var indexSlide = activeSlide.className.split(" ")[1].split("--")[1];
-  var indexSlideParse = parseInt(indexSlide);
-  console.log("indexParse" + indexSlideParse);
-  var scroll = e.wheelDelta;
-
-  if (spam == false) {
-    //Scroll vers le haut
-    if (scroll > 0 && scroll < 1000 && indexSlideParse > 1) {
-
-      var timer = null;
-      if (timer) {
-        clearTimeout(timer);
-      }
-      var timer = setTimeout(() => {
-        timer = null;
-        spam = true;
-        activeSlide.classList.remove("activeSlideProject");
-        indexSlideParse--;
-
-        var active = document.querySelector(".slideProject--" + indexSlideParse);
-        active.classList.add("activeSlideProject");
-
-        setTimeout(() => {
-          spam = false;
-        }, 1000)
-      }, 500)
-    }
-    //scroll vers le bas
-    if (scroll < 0 && scroll > -1000 && indexSlideParse < slideProject.length) {
-      var timer = null;
-      if (timer) {
-        clearTimeout(timer);
-      }
-      var timer = setTimeout(() => {
-        timer = null;
-        spam = true;
-        activeSlide.classList.remove("activeSlideProject");
-        indexSlideParse++;
-
-        var active = document.querySelector(".slideProject--" + indexSlideParse);
-        active.classList.add("activeSlideProject");
-
-        setTimeout(() => {
-          spam = false;
-        }, 1000)
-      }, 500)
-    }
-  }
 });
